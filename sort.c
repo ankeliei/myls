cf190ab48329d63e16c3fab5efeda87fe0a3435e
Ankel <2533156094@qq.com>
2020-07-29 18:01:06 +0800

    进行时间排序--还存在问题

--- sort.c
+++ sort.c
@@ -53,7 +53,6 @@ int view1(char *str, int r, int show_hidden, int view_mod, char * r_head) {     
         }
     }
 
-    
     for(i=0; i<n; i++){
         char *filename = tmp[i].d_name;
         struct stat get_message = {};        //准备好用于接收stat的的结构体
@@ -142,7 +141,159 @@ int view1(char *str, int r, int show_hidden, int view_mod, char * r_head) {     
 }
 
 int view2(char *str, int r, int show_hidden, int view_mod, char * r_head) {     //进行时间排序输出
-    
+    DIR * dir;
+    struct dirent *ptr;
+
+    if((dir=opendir(str)) == NULL) //打开文件流指针
+    {
+        fprintf(stderr,"cannot open directory:%s\n",str);
+        return -1;
+    }
+
+    int n = 0;
+
+    while( (ptr = readdir(dir)) !=NULL ){
+        if(strcmp(ptr->d_name, ".") && strcmp(ptr->d_name, "..")) {
+            n = n + 1;
+        }
+    }
+    closedir(dir);
+    struct dirent tmp[n], t;
+    long *mtime[n], tt;          //存储时间戳的数组
+
+    n = 0;
+    dir=opendir(str);
+    while((ptr = readdir(dir)) != NULL)     //遍历出ptr存储在tmp[]中，不包括.和..文件
+    {
+        char *filename = ptr->d_name;
+        struct stat get_message = {};        //准备好用于接收stat的的结构体
+        chdir(str);
+        stat(filename, &get_message);       //读出stat
+
+        if(strcmp(filename, ".") && strcmp(filename, "..")) {
+            memcpy(&tmp[n], ptr, sizeof(tmp[n]));
+            mtime[n] = (long)ctime(&(get_message.st_mtime));
+            //printf("%ld\n",mtime[n]);
+            n = n + 1;
+        }
+    }
+
+    int i=0;
+    for( i=1; i<n; i++){           //至此完成排序存储在tmp[]中
+        int j = 0;
+        for(j=0; j<i; j++){
+            if(strcmp(tmp[i].d_name, tmp[j].d_name) < 0) {
+                memcpy(&t, &tmp[j], sizeof(tmp[n]));
+                memcpy(&tmp[j], &tmp[i], sizeof(tmp[n]));
+                memcpy(&tmp[i], &t, sizeof(tmp[n]));
+                
+                tt = mtime[j];
+                mtime[j] = mtime[i];
+                mtime[i] = tt;
+            }
+        }
+    }
+
+    for( i=1; i<n; i++){           //排序存储在tmp[]中
+        int j = 0;
+        for(j=0; j<i; j++){
+            if(mtime[i] < mtime[j] ) {
+                memcpy(&t, &tmp[j], sizeof(tmp[n]));
+                memcpy(&tmp[j], &tmp[i], sizeof(tmp[n]));
+                memcpy(&tmp[i], &t, sizeof(tmp[n]));
+
+                tt = mtime[j];
+                mtime[j] = mtime[i];
+                mtime[i] = tt;
+            }
+        }
+    }
+
+    for(i=0; i<n; i++){
+        char *filename = tmp[i].d_name;
+        struct stat get_message = {};        //准备好用于接收stat的的结构体
+        chdir(str);
+        stat(filename, &get_message);       //读出stat
+
+        if(show_hidden == 0)
+        {
+            if(filename[0] != '.')
+            {
+                if(r)
+                {
+                    if(tmp[i].d_type == '\004')
+                    {
+                        int n1 = strlen(str) + strlen(filename) + strlen("/") + 1;
+                        int n2 = strlen(r_head) + strlen("/") + strlen("/") + 1;
+
+                        char *r_str = (char *)malloc( n1 );
+                        sprintf(r_str, "%s/%s", str, filename);
+                    
+                        char r_str_t[n1];             //设置暂存字符串接收malloc分配出来的内容
+                        strcpy(r_str_t, r_str);
+
+                        char *new_r_head = (char *)malloc( n2 );
+                        sprintf(new_r_head, "%s--", r_head);
+                    
+                        char new_r_head_t[n2];      //设置暂存字符串接收malloc分配出来的内容
+                        strcpy(new_r_head_t, new_r_head);
+
+                        list_message(filename, &get_message, view_mod, r_head);
+                        view1(r_str_t, r, show_hidden, view_mod, new_r_head_t);
+                        free(new_r_head);
+                        free(r_str);
+                    }
+                    else
+                    {
+                        list_message(filename, &get_message, view_mod, r_head);
+                    }
+                }
+                else
+                {
+                    list_message(filename, &get_message, view_mod, r_head);
+                }
+            }
+        }
+        if(show_hidden)
+        {
+            if(r)
+            {
+                if(tmp[i].d_type == '\004')
+                {
+                    int n1 = strlen(str) + strlen(filename) + strlen("/") + 1;
+                    int n2 = strlen(r_head) + strlen("/") + strlen("/") + 1;
+
+                    char *r_str = (char *)malloc( n1 );
+                    sprintf(r_str, "%s/%s", str, filename);
+
+                    char r_str_t[n1];             //设置暂存字符串接收malloc分配出来的内容
+                    strcpy(r_str_t, r_str);
+
+                    char *new_r_head = (char *)malloc( n2 );
+                    sprintf(new_r_head, "%s--", r_head);
+                    
+                    char new_r_head_t[n2];      //设置暂存字符串接收malloc分配出来的内容
+                    strcpy(new_r_head_t, new_r_head);
+
+                    list_message(filename, &get_message, view_mod, r_head);
+                    view1(r_str_t, r, show_hidden, view_mod, new_r_head_t);
+                    free(new_r_head);
+                    free(r_str);
+                }
+                else
+                {
+                    list_message(filename, &get_message, view_mod, r_head);
+                }   
+            }
+            else
+            {
+                list_message(filename, &get_message, view_mod, r_head);
+            }
+        }
+    }
+
+    close(dir);
+    return 0;
 };
 
 int view3(char *str, int r, int show_hidden, int view_mod){};
